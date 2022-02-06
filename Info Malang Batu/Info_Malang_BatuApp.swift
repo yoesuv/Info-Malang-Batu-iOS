424d90a601dee2eb5ede0c92ef4d37ebbb37feec
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2022-02-06 07:43:45 +0700

    Initial Commit

---
+++ Info Malang Batu/Info_Malang_BatuApp.swift
@@ -0,0 +1,17 @@
+//
+//  Info_Malang_BatuApp.swift
+//  Info Malang Batu
+//
+//  Created by Yusuf Saifudin on 06/02/22.
+//
+
+import SwiftUI
+
+@main
+struct Info_Malang_BatuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
