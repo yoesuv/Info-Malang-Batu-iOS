fdfabe7b05a32d5b15197a27cf41e044094639fa
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2022-02-10 09:46:23 +0700

    :pencil: load api key from file

--- Info Malang Batu/Views/TabViewMaps.swift
+++ Info Malang Batu/Views/TabViewMaps.swift
@@ -16,9 +16,23 @@ struct TabViewMaps: View {
             .navigationTitle("Maps")
             .navigationBarTitleDisplayMode(.inline)
         }
+        .onAppear {
+            loadApiKeys()
+        }
         .navigationTitle("")
         .navigationBarHidden(true)
     }
+    
+    func loadApiKeys() {
+        if let path = Bundle.main.path(forResource: "keys", ofType: "plist") {
+            if let dict = NSDictionary(contentsOfFile: path) {
+                if let apiKey = dict["google_maps_api_key"] as? String {
+                    print("TabViewMaps # google maps api key \(apiKey)")
+                }
+            }
+        }
+    }
+    
 }
 
 struct TabViewMaps_Previews: PreviewProvider {
