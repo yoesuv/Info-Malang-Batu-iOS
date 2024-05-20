d6129f0587a3cc07513e4b0b132a9f3c53ef6600
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2024-05-20 09:46:04 +0700

    :newspaper: add default unit test

---
+++ Info Malang BatuTests/Info_Malang_BatuTests.swift
@@ -0,0 +1,30 @@
+//
+//  Info_Malang_BatuTests.swift
+//  Info Malang BatuTests
+//
+//  Created by Yusuf Saifudin on 20/05/24.
+//
+
+import XCTest
+
+final class Info_Malang_BatuTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        
+    }
+
+    override func tearDownWithError() throws {
+        
+    }
+
+    func testExample() throws {
+        
+    }
+
+    func testPerformanceExample() throws {
+        measure {
+            
+        }
+    }
+
+}
